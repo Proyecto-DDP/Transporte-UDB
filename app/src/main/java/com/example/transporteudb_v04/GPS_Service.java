6bb987cbda8d29f00079c22730147de688f4b242
Ricardo <inuriqui_cool2@outlook.com>
2019-09-24 22:01:38 -0600

    Perdoname Diego

--- app/src/main/java/com/example/transporteudb_v04/GPS_Service.java
+++ app/src/main/java/com/example/transporteudb_v04/GPS_Service.java
@@ -1,4 +1,15 @@
 package com.example.transporteudb_v04;
 
-public class GPS_Service {
+import android.app.Service;
+import android.content.Intent;
+import android.os.IBinder;
+
+import androidx.annotation.Nullable;
+
+public class GPS_Service extends Service {
+    @Nullable
+    @Override
+    public IBinder onBind(Intent intent) {
+        return null;
+    }
 }
