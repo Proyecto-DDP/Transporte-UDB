1ddedb128840ffa98f908bf85bde6bb9b1768a3c
Diego <diegofytn@gmail.com>
2019-09-28 20:18:05 -0600

    Resurgiendo como el ave Fénix

---
+++ app/src/main/java/proyecto/transporte/udb/MainActivity.java
@@ -0,0 +1,31 @@
+package proyecto.transporte.udb;
+
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.widget.Toolbar;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        Toolbar toolbar = findViewById(R.id.toolbar_main);
+        setSupportActionBar(toolbar);
+        getSupportActionBar().setDisplayShowTitleEnabled(false);
+    }
+    public void showInfo(View view){
+        Intent intent = new Intent(this, show_info.class);
+        startActivity(intent);
+    }
+    public  void showMapa(View view){
+        Intent intent = new Intent(this, MapsActivity.class);
+        startActivity(intent);
+
+    }
+
+}
