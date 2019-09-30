4ce87cf13a167890884f94e720b49ff945b73274
oscarp3112 <oscarp1998@hotmail.com>
2019-09-29 20:32:16 -0600

    Login Funcional

--- app/src/main/java/proyecto/transporte/udb/Login.java
+++ app/src/main/java/proyecto/transporte/udb/Login.java
@@ -1,21 +1,105 @@
 package proyecto.transporte.udb;
-import androidx.appcompat.app.AppCompatActivity;
-import androidx.appcompat.widget.Toolbar;
 
+import android.content.DialogInterface;
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.EditText;
+
+import androidx.appcompat.app.AlertDialog;
+import androidx.appcompat.app.AppCompatActivity;
+
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.ValueEventListener;
+
 
 public class Login extends AppCompatActivity {
+
+    private FirebaseDatabase database;
+    private DatabaseReference referencia;
+
+
+    public String username;
+    public String password;
+
+    private EditText Usuario;
+    private EditText Password;
+
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
+        Usuario = findViewById(R.id.carnet);
+        Password = findViewById(R.id.pass);
+
+        database = FirebaseDatabase.getInstance();
+        referencia = database.getReference();
+
     }
+
+
+    public void Identify(String uID, final String uPASS){
+
+        referencia.child("Usuarios").child(uID).addListenerForSingleValueEvent(
+                new ValueEventListener () {
+                    @Override
+                    public void onDataChange(DataSnapshot dataSnapshot) {
+                        // Lo ENCUENTRA
+                        String TruePassword = dataSnapshot.child("Pass").getValue(String.class);
+                        if (uPASS.equals(TruePassword))
+                        {
+                            ActividadSig();
+                        }
+                        else
+                        {
+                            CrearDialogo("Error", "Usuario o password incorrecto");
+
+                        }
+                    }
+
+                    @Override
+                    public void onCancelled(DatabaseError databaseError) {
+                        // Por si falla
+                        CrearDialogo("Error", "Usuario o password incorrecto");
+                    }
+                });
+    }
+
+
+
 
     //En respuesta al botón
     public void nextActivity(View view) {
+        // Intent intent = new Intent(this, MainActivity.class);
+
+        // startActivity(intent);
+
+      String  username = Usuario.getText().toString();
+      String  password = Password.getText().toString();
+      //  CrearDialogo(username, password);
+
+        Identify(username, password);
+    }
+
+    public void CrearDialogo(String titulo, String mensaje) {
+        AlertDialog.Builder builder = new AlertDialog.Builder(this);
+        builder.setTitle(titulo);
+        builder.setMessage(mensaje);
+        builder.setPositiveButton("Aceptar", new DialogInterface.OnClickListener() {
+            @Override
+            public void onClick(DialogInterface dialog, int which) {
+            }
+        });
+        builder.show();
+    }
+
+    public void ActividadSig()
+    {
         Intent intent = new Intent(this, MainActivity.class);
         startActivity(intent);
     }
