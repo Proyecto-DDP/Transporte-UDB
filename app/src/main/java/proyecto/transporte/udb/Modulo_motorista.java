a9890abcbde50413b221abbe1e589f4f5fbc29ee
Spinal29 <ricardoespinal@hotmail.com>
2019-09-30 19:43:04 -0600

    Modulo java

---
+++ app/src/main/java/proyecto/transporte/udb/Modulo_motorista.java
@@ -0,0 +1,14 @@
+package com.example.transporteudb_v04;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class Modulo_motorista extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_modulo_motorista);
+    }
+}
