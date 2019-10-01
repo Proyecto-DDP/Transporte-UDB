d765a66b23e937067ea800899fb92c87fc97cec1
Diego <diegofytn@gmail.com>
2019-09-30 21:05:37 -0600

    Añadiendo el login

--- app/src/main/java/proyecto/transporte/udb/Login.java
+++ app/src/main/java/proyecto/transporte/udb/Login.java
@@ -4,7 +4,7 @@ import android.content.DialogInterface;
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
-import android.widget.EditText;
+import android.widget.EditText; 
 
 import androidx.appcompat.app.AlertDialog;
 import androidx.appcompat.app.AppCompatActivity;
