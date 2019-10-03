409637233a196635cf278bfce3f50e2ce22449d4
Diego <diegofytn@gmail.com>
2019-10-02 20:49:15 -0600

    Agregando pantalla para mantenimiento de zonas

--- app/src/main/java/proyecto/transporte/udb/mant_zona.java
+++ app/src/main/java/proyecto/transporte/udb/mant_zona.java
@@ -2,24 +2,17 @@ package proyecto.transporte.udb;
 
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.appcompat.widget.Toolbar;
-
-import android.content.Intent;
 import android.os.Bundle;
-import android.view.View;
 
-public class AdminActivity extends AppCompatActivity {
+public class mant_zona extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_admin);
+        setContentView(R.layout.activity_mantzona);
 
-        Toolbar toolbar = findViewById(R.id.toolbar_main);
+        Toolbar toolbar = findViewById(R.id.toolbar_back);
         setSupportActionBar(toolbar);
         getSupportActionBar().setDisplayShowTitleEnabled(false);
-    }
-
-    public void mantZona(View view){
-        Intent intent = new Intent(this, mant_zona.class);
-        startActivity(intent);
+        getSupportActionBar().setDisplayHomeAsUpEnabled(true);
     }
 }
